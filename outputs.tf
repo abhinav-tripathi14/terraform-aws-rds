0ddfaf115db499e55d893d621d7f6f79f5083245
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-07-29 14:49:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.education.address
@@ -12,4 +13,4 @@ output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.education.username
 }
-
+*/
